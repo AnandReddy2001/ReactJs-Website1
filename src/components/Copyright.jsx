856f883cbb72ba8cc6042cad828dc74cf7e5ec56
anand7001133 <anand@ramailo.tech>
2024-03-21 23:33:36 +0530

    Copyright

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Copyright = () => {
   return (
     <div className="bg-[#2699fb] text-white text-xs sm:text-sm md:text-base p-4 border-t font-bold border-white text-center">
-      Copyright - &copy; Anand Reddy - 2024
+      &copy; 2024-Anand Reddy. All rights reserved.
     </div>
   );
 };
