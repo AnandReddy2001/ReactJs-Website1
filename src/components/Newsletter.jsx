c1d10788385b1918dd553669a8e769d7c4707e39
anand7001133 <anand@ramailo.tech>
2024-03-21 13:47:34 +0530

    footer

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,54 +1,73 @@
 import React, { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const Newsletter = () => {
-  const [email, setEmail] = useState("");
-  const [submittedEmails, setSubmittedEmails] = useState([]);
+  const [newData, setNewData] = useState([]);
 
-  const handleSubmit = () => {
-    console.log("Email submitted:", email);
-    setSubmittedEmails([...submittedEmails, email]);
-    setEmail("");
-    alert("Email is submitted");
+  const initialValues = {
+    email: "",
   };
 
-  const handleChange = (event) => {
-    setEmail(event.target.value);
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .trim()
+      .email("Invalid email address")
+      .required("Email is required"),
+  });
+
+  const handleSubmit = (values, { resetForm }) => {
+    console.log("Email submitted:", values.email);
+    setNewData([...newData, values.email]);
+    resetForm();
+    alert("Email is submitted");
   };
 
   return (
     <div className="bg-[#2699fb] md:py-[80px] py-[50px] px-[10px] md:px-0">
-      <div className="md:flex justify-between ">
-        <div className="text-white  pb-[20px] mx-auto max-w-[90%]">
-          <h1 className="md:text-[40px] text-[20px] font-bold ">
-            Want to learn latest I.T skills ?
+      <div className="md:flex justify-between">
+        <div className="text-white pb-[20px] mx-auto max-w-[90%]">
+          <h1 className="md:text-[40px] text-[20px] font-bold">
+            Want to learn the latest I.T skills?
           </h1>
-          <span>Sign up our newsletter and stay up to date.</span>
+          <span>Sign up for our newsletter and stay up to date.</span>
         </div>
-        <div className="mx-auto  max-w-[90%] ">
-          <div className="md:flex ">
-            <div className="flex-colflex ">
-              <input
-                type="email"
-                value={email}
-                onChange={handleChange}
-                className="md:p-3 p-2 mr-1 text-slate-600 outline-none rounded w-64 h-10"
-                placeholder="Email..."
-              />
-            </div>
+        <div className="mx-auto max-w-[90%]">
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ isSubmitting }) => (
+              <Form className="md:flex">
+                <div className="flex-col flex">
+                  <Field
+                    type="email"
+                    name="email"
+                    className="md:p-3 p-2 mr-1 text-slate-600 outline-none rounded w-64 h-10"
+                    placeholder="Email@company.com"
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="text-red-500"
+                  />
+                </div>
 
-            <button
-              type="button"
-              disabled={email === ""}
-              onClick={handleSubmit}
-              className="bg-black text-white md:p-2 p-2 md:mt-0 mt-2 rounded h-10 w-28 "
-            >
-              Notify Me
-            </button>
-          </div>
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="bg-black text-white md:p-2 p-2 md:mt-0 mt-2 rounded h-10 w-28"
+                >
+                  Notify Me
+                </button>
+              </Form>
+            )}
+          </Formik>
           <br />
-          <p className="text-white ">
-            We care about the protection of your data. Read out <br />{" "}
-            <a href="" className="text-black f">
+          <p className="text-white">
+            We care about the protection of your data. Read our <br />
+            <a href="#" className="text-black">
               Privacy Policy
             </a>
           </p>
